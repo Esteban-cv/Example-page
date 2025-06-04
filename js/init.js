b073fcbab7695ab42988f8c0f118bd6efb80b06f
Esteban C.v <stphgamer@gmail.com>
2025-06-04 18:47:49 -0500

    Queda pendiente terminar
    no se termino el footer y faltan porcentajes y conclusiones

--- js/init.js
+++ js/init.js
@@ -5,3 +5,27 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+   document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
